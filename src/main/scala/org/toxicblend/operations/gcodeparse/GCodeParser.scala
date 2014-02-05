f3740f7dd59be62da607a66551422f2c368e9bb9
EAD Fritz <ead.fritz@gmail.com>
2014-02-05 22:06:52 +0100

    renamed some parser methods, added tests

--- src/main/scala/org/toxicblend/operations/gcodeparse/GCodeParser.scala
+++ src/main/scala/org/toxicblend/operations/gcodeparse/GCodeParser.scala
@@ -6,30 +6,45 @@ import toxi.geom.Vec3D
 import scala.collection.mutable.ArrayBuffer
 import scala.annotation.migration
 
-class GcodeStepArgument(val key:String, val value:Float) {
+class GcodeParameter(val key:String, val value:Float) {
   override def toString = key+value
 }
 
-class GcodeStep(val stepData:List[GcodeStepArgument]) {
-  override def toString = stepData.toString
+class GcodeParameters(val parameters:List[GcodeParameter]) {
+  override def toString = parameters.toString
 }
 
-class GcodeCommand(val key:String, val steps:List[GcodeStep]) {
+/**
+ * base class for single and multi line commands
+ */
+class GcodeLine
+/**
+ * Gcode command where the parameters only can occur once
+ */
+class GcodeSingleCommand(val key:String,val parameters:Option[GcodeParameters]) extends GcodeLine {
+  override def toString = key
+}
+/**
+ * Gcode command where the parameters only can occur more than once (separate lines though)
+ */
+class GcodeMultiCommand(val key:String, val steps:List[GcodeParameters]) extends GcodeLine {
   override def toString = "key:" + key + " " + steps
 }
 
-class GcodeCommands(val commands:List[GcodeCommand]) {
+class GcodeLines(inputCommands:List[List[GcodeLine]]) {
+  val commands = inputCommands.flatten
   override def toString = commands.toString
   /**
    * Generates a sequence of state, on step for each active line in the gcode file
    */
   def getSegments:IndexedSeq[Segment] = {
      val rv = new ArrayBuffer[Segment]
-     val state = new State(new Vec3D,new Vec3D, 0, 0)
-     commands.foreach(command => {
+     val state = new State(new Vec3D, new Vec3D, 0, 0)
+     commands.foreach(aCommand => aCommand match {
+       case command:GcodeMultiCommand => 
          if (command.key=="G0" || command.key=="G1") {
            command.steps.foreach(step => {
-             step.stepData.foreach( argument => {
+             step.parameters.foreach( argument => {
                argument.key match {
                  case "F" => if (command.key == "G0") state.f0 = argument.value else state.f1 = argument.value
                  case "X" => state.p1.x = argument.value
@@ -44,6 +59,7 @@ class GcodeCommands(val commands:List[GcodeCommand]) {
              state.p0.z = state.p1.z
            })
          }
+       case setting:GcodeSingleCommand => println("ignoring setting" + setting)
      })
      rv
   }
@@ -54,69 +70,84 @@ class GcodeCommands(val commands:List[GcodeCommand]) {
 }
 
 /** 
- *  Parses the gcode text into an object hierarchy
+ * Parses the gcode text into an object hierarchy.
+ * G0 and G1 commands can look like this:
+ * G0 X1 Y2 Z1
+ *    X2 
+ *    X3
+ *    ..
+ *    
+ * 'Settings' can sometimes occur on the same line:
+ * G40 G49 G54 G80 G90 G94
+ * 
+ * I make the assumtion that multiple G0 and G1 commands never occupy the same line  
  */
 class GCodeParser extends RegexParsers {
   val NEWLINE = String.format("%n") // """\n""" is sometimes """\r\n""" :/
   override val skipWhitespace = false
-      
-  def gcodeCommands: Parser[GcodeCommands] = lineNumber.? ~> rep1sep(gcodeCommand,NEWLINE) ^^ {  
-    case commands => new GcodeCommands(commands)
+  
+  def gCode: Parser[GcodeLines] = rep((rep1sep(gMultiLineCommand,NEWLINE) | rep1(gSingleLineCommand))<~NEWLINE.? ) ^^ {
+    case lines => new GcodeLines(lines)
   }
   
-  def gcodeCommand: Parser[GcodeCommand] = lineNumber.? ~> (gCommandName ~ gcodeSteps) ^^ {
-    case gCommandName~gArgumentsLines => new GcodeCommand(gCommandName.capitalize,gArgumentsLines)
+  /**
+   * multiline commands (G0 and G1)
+   */ 
+  def gMultiLineCommand : Parser[GcodeLine] = """(?i)(?:G0?0)|(?:G0?1)""".r ~ gMultiLineParameters ^^ { 
+    case name~parameters => new GcodeMultiCommand(name.capitalize,parameters)
   }
   
-  def gCommandName =  """(?i)(?:G0?0)|(?:G0?1)""".r
-  // any G except G0 and G1.  G0 and G1 should always be separated by a newline (imho)
-  def gSettingName =  """(?i)(?:G[123456789]?\d)(?:G0?[23456789])|(?:M\d?\d?\d)""".r 
+  /** 
+   * single line commands . Any G except G0 and G1. G0 and G1 should always be separated by a newline (imho)
+   */
+  def gSingleLineCommand : Parser[GcodeLine] = ( """(?i)(?:G[123456789]\d)|(?:G0?[23456789])|(?:M\d?\d?\d)""".r ~ gSingleLineParameters.?) ^^ { 
+    case name~parameters => new GcodeSingleCommand(name,parameters)
+  }
 
   /**
-   * A list command steps. e.g. matching:"X43.2 F1000 Z-1\nX2 Z1"
+   * A list (potentially multi line) command steps. e.g. matching:"X43.2 F1000 Z-1  \n  X2 Z1"
    */
-  def gcodeSteps: Parser[List[GcodeStep]] = rep1sep(gcodeLine,NEWLINE) 
+  def gMultiLineParameters: Parser[List[GcodeParameters]] = rep1sep(gSingleLineParameters,NEWLINE) 
   
   /**
-   * A list of command arguments representing one step. e.g. matching:"X43.2 F1000 Z-1"
+   * A list of single line command arguments representing one step. e.g. matching:"X43.2 F1000 Z-1"
    */
-  def gcodeLine: Parser[GcodeStep] = lineNumber.? ~> rep1(gcodeStepArgument) ^^ { 
-    case arguments => new GcodeStep(arguments)
+  def gSingleLineParameters: Parser[GcodeParameters] = rep1(gParameter) ^^ { 
+    case arguments => new GcodeParameters(arguments)
   } 
   
   /**
    * a single argument to the commands. e.g matching: "X43.2", "F1000", "Z-1"
    */
-  def gcodeStepArgument:Parser[GcodeStepArgument] = (gArgumentKey ~ fNumber) ^^ {
-    case gArgumentKey~fNumber => new GcodeStepArgument(gArgumentKey.capitalize,fNumber) 
+  def gParameter:Parser[GcodeParameter] = ("""(?i)[XYZIJFPQS]""".r ~ gParameterValue) ^^ {
+    case gArgumentKey~fNumber => new GcodeParameter(gArgumentKey.capitalize,fNumber) 
   }
-  def gArgumentKey = """(?i)[XYZIJFPQS]""".r
-  def fNumber: Parser[Float] = """-?\d+(?:\.(?:\d*)?)?(?:[E|e]-?\d+)?""".r ^^ { _.toFloat }
-  def lineNumber = """(?i)N\d+""".r
-  //def emptyline = "^/n".r
+  
+  def gParameterValue: Parser[Float] = """-?\d+(?:\.(?:\d*)?)?(?:[E|e]-?\d+)?""".r ^^ { _.toFloat }
   
   /**
-   * remove non-nested comments and whitespace
+   * remove non-nested comments and other whitespace
    * This is done in a super inefficient two pass. TODO: figure out how to avoid these steps
    */
   def filterOutWhiteSpace(input:String) = {
     val tmp = """(?s)(?:[ \t\r\f]+)|(?:\([^\)]*\))""".r.replaceAllIn(input, "") // remove non-nested comments and whitespace
-    """(?m)(?:^\n)+""".r.replaceAllIn(tmp, "") // remove empty lines
+    """(?i)(?m)(?:^\n)|(?:^N\d+)+""".r.replaceAllIn(tmp, "") // remove empty lines and line numbers
   }
 }
 
 object GCodeParser {
   def main(args: Array[String]) {
    val objParser = new GCodeParser
-   var arg1 = "N0G0 Z2.0 (goto safe z)\nn0g0  x1 y2  z3 (comme\nnt comment)\n  N  1 g1 x2 y2 z3"
+   val arg0 = "G17 G20 G40 G49 G54 G80 G90 G94"
+   var arg1 = "\n\n\nn1G0 Z2.0x2y0 (goto safe z)\nn2g0  x1 y2  z3 (comme\nnt comment)\nn3 g1 x2 y2 z3\n x1\nz0"
    var arg = """
-G0 Z2.0 (goto safe z)
-M3 S1000 (start splindle)
+     N0G17 G20 G40 G49 G54 G80 G90 G94
+G0 Z2.0 (goto safe z) x0
 G4 P3  (dwell 3 seconds)
 G0 F1000 (set rapid feedrate)
 G1 F500 (set normal feedrate)
 G64 P0.02 Q0.02
-G0 Z2.000000
+G0 Z2.000000 x2
 G0 X19.01426 Y26.41212
 G1 X19.01426 Y26.41212 Z0 F500.000000
 G1 X19.01426 Y26.41212 Z-11.68791 F100.000000
@@ -125,19 +156,19 @@ G1 X19.01426 Y26.41212 Z-11.68791
  X19.01426 Y26.41212 Z-11.68791
 G1 X20.81396 Y27.20154 Z-13.37177 
 G1 X23.99418 Y24.75371 Z-11.88044 
-G1 X26.76230 Y23.35208 Z-10.87037 
-G1 X30.86569 Y22.06351 Z-9.78996 
-G1 X35.54356 Y21.31058 Z-9.02444 
+ X26.76230 Y23.35208 Z-10.87037 
+ X30.86569 Y22.06351 Z-9.78996 
+ X35.54356 Y21.31058 Z-9.02444 
 G1 X40.85203 Y21.03279 Z-8.58315 
 G1 X48.17540 Y21.27965 Z-8.31863 
-G1 X61.07601 Y22.72789 Z-7.92359 
-"""
+G1 X61.07601 Y22.72789 Z-7.92359"""
    arg = objParser.filterOutWhiteSpace(arg)
-   val rv = objParser.parse(objParser.gcodeCommands, arg)
-   println("rest:" + rv.next.source.toString.substring(rv.next.offset))
+   val rv = objParser.parse(objParser.gCode, arg)
+   println("rest:>" + rv.next.source.toString.substring(rv.next.offset) + "<")
+   println()
    val objects = rv.get
    println("input : " + arg + "\noutput:" + objects)
    //println("output : " + objects + " len:" + objects.getSegments.size)
-   //println("output : \n" + objects.getSegments.mkString("\n"))
+   println("output : \n" + objects.getSegments.mkString("\n"))
   }
 }
--- src/test/scala/org/toxicblend/tests/GCodeParserTests1.scala
+++ src/test/scala/org/toxicblend/tests/GCodeParserTests1.scala
@@ -3,51 +3,29 @@ package org.toxicblend.tests
 import collection.mutable.Stack
 import org.scalatest._
 import org.toxicblend.operations.gcodeparse.GCodeParser
-import org.toxicblend.operations.gcodeparse.GcodeCommand
-import org.toxicblend.operations.gcodeparse.GcodeCommands
-import org.toxicblend.operations.gcodeparse.GcodeStepArgument
+import org.toxicblend.operations.gcodeparse.GcodeMultiCommand
+import org.toxicblend.operations.gcodeparse.GcodeSingleCommand
+import org.toxicblend.operations.gcodeparse.GcodeLines
+import org.toxicblend.operations.gcodeparse.GcodeParameter
 
 class GCodeParserTests1 extends FlatSpec with Matchers {
-  
+ 
   "regexp test" should "work" in {
      val r = """(?:[ \t\r]+)|(?:\([^\)]*\))""".r.replaceAllIn("  h i(fan)h(keep h)(d s \ndfg ) a a", "")  
      r.size should be (5)
      r should be ("hihaa")
   }
   
-  "simple line wo comment" should "completely parse" in {
+  "gSingleLineParameters wo comment" should "completely parse" in {
     val objParser = new GCodeParser
     val arg = "x1 y2 z3"
-    val parsed = objParser.parse(objParser.gcodeSteps,objParser.filterOutWhiteSpace(arg))
+    val parsed = objParser.parse(objParser.gSingleLineParameters,objParser.filterOutWhiteSpace(arg))
     val ret = parsed.get
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
     rest.size should be (0)
     
-    val steps = ret.toArray 
-    steps.size should be (1)
-    val stepData = steps(0).stepData.toArray
-    stepData.size should be (3)
-    stepData(0).key   should be ("X")
-    stepData(0).value should be (1f)
-    stepData(1).key   should be ("Y")
-    stepData(1).value should be (2f)
-    stepData(2).key   should be ("Z")
-    stepData(2).value should be (3f)
-  }
-
-  "simple line with comment" should "completely parse" in {
-    val objParser = new GCodeParser
-    val arg = "x1 y2 z3 (comment1)"
-    val parsed = objParser.parse(objParser.gcodeSteps,objParser.filterOutWhiteSpace(arg))
-    val ret = parsed.get
-    val rest = parsed.next.source.toString.substring(parsed.next.offset)
-    if (rest.size > 0) println("unparsed:" + rest)
-    rest.size should be (0)
-    
-    val steps = ret.toArray 
-    steps.size should be (1)
-    val stepData = steps(0).stepData.toArray
+    val stepData = ret.parameters.toArray 
     stepData.size should be (3)
     stepData(0).key   should be ("X")
     stepData(0).value should be (1f)
@@ -57,18 +35,37 @@ class GCodeParserTests1 extends FlatSpec with Matchers {
     stepData(2).value should be (3f)
   }
   
-  "simple gcode line wo comment" should "completely parse" in {
+  "gSingleLineParameters with comment" should "completely parse" in {
+    val objParser = new GCodeParser
+    val arg = "x5 y6 (this is a comment)z7(this is too)"
+    val parsed = objParser.parse(objParser.gSingleLineParameters,objParser.filterOutWhiteSpace(arg))
+    val ret = parsed.get
+    val rest = parsed.next.source.toString.substring(parsed.next.offset)
+    if (rest.size > 0) println("unparsed:" + rest)
+    rest.size should be (0)
+    
+    val stepData = ret.parameters.toArray 
+    stepData.size should be (3)
+    stepData(0).key   should be ("X")
+    stepData(0).value should be (5f)
+    stepData(1).key   should be ("Y")
+    stepData(1).value should be (6f)
+    stepData(2).key   should be ("Z")
+    stepData(2).value should be (7f)
+  }
+
+  "gMultiLineCommand wo comment" should "completely parse" in {
     val objParser = new GCodeParser
     val arg = "g0  x1 y2 z3"
-    val parsed = objParser.parse(objParser.gcodeCommand,objParser.filterOutWhiteSpace(arg))
-    val ret = parsed.get
+    val parsed = objParser.parse(objParser.gMultiLineCommand,objParser.filterOutWhiteSpace(arg))
+    val ret = parsed.get match { case rv:GcodeMultiCommand => rv }
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
     rest.size should be (0)
     
-    val steps = ret.steps 
+    val steps = ret.steps.toArray 
     steps.size should be (1)
-    val stepData = steps(0).stepData.toArray
+    val stepData = steps(0).parameters.toArray
     ret.key should be ("G0")
     stepData.size should be (3)
     stepData(0).key   should be ("X")
@@ -79,38 +76,41 @@ class GCodeParserTests1 extends FlatSpec with Matchers {
     stepData(2).value should be (3f)
   }
   
-  "simple gcode line with comment" should "completely parse" in {
+  "gMultiLineCommand with comment" should "completely parse" in {
     val objParser = new GCodeParser
-    val arg = "g0  x1 y2  z3 (comme\nnt comment)"
-    val parsed = objParser.parse(objParser.gcodeCommand,objParser.filterOutWhiteSpace(arg))
-    val ret = parsed.get
+    val arg = "(comment)g0()  x4 y5 z6(comment)"
+    val parsed = objParser.parse(objParser.gMultiLineCommand,objParser.filterOutWhiteSpace(arg))
+    val ret = parsed.get match { case rv:GcodeMultiCommand => rv }
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
     rest.size should be (0)
-    val steps = ret.steps 
+    
+    val steps = ret.steps.toArray 
     steps.size should be (1)
-    val stepData = steps(0).stepData.toArray
+    val stepData = steps(0).parameters.toArray
     ret.key should be ("G0")
     stepData.size should be (3)
     stepData(0).key   should be ("X")
-    stepData(0).value should be (1f)
+    stepData(0).value should be (4f)
     stepData(1).key   should be ("Y")
-    stepData(1).value should be (2f)
+    stepData(1).value should be (5f)
     stepData(2).key   should be ("Z")
-    stepData(2).value should be (3f)
+    stepData(2).value should be (6f)
   }
   
-  "gcode multiline with comment" should "completely parse" in {
+  "gMultiLineCommand multiline with comment" should "completely parse" in {
     val objParser = new GCodeParser
-    val arg = "g0  x1 y2  z3 (comme\nnt comment)\nx2 y2 z3"
-    val parsed = objParser.parse(objParser.gcodeCommand,objParser.filterOutWhiteSpace(arg))
-    val ret = parsed.get
+    val arg = "n0g0  x1 y2  z3 (comme\nnt comment)\nx2 y2 (ffdf)z-3(dfgdf)"
+    val parsed = objParser.parse(objParser.gMultiLineCommand,objParser.filterOutWhiteSpace(arg))
+    val ret = parsed.get match { case rv:GcodeMultiCommand => rv }
+
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
     rest.size should be (0)
-    val steps = ret.steps 
+    
+    val steps = ret.steps.toArray 
     steps.size should be (2)
-    var stepData = steps(0).stepData.toArray
+    var stepData = steps(0).parameters.toArray
     ret.key should be ("G0")
     stepData.size should be (3)
     stepData(0).key   should be ("X")
@@ -119,74 +119,114 @@ class GCodeParserTests1 extends FlatSpec with Matchers {
     stepData(1).value should be (2f)
     stepData(2).key   should be ("Z")
     stepData(2).value should be (3f)
-    stepData = steps(1).stepData.toArray
+    stepData = steps(1).parameters.toArray
     stepData.size should be (3)
     stepData(0).key   should be ("X")
     stepData(0).value should be (2f)
     stepData(1).key   should be ("Y")
     stepData(1).value should be (2f)
     stepData(2).key   should be ("Z")
-    stepData(2).value should be (3f)
+    stepData(2).value should be (-3f)
   }
   
-  "multigcode multiline with comment" should "completely parse" in {
+  "gSingleLineCommand with comment" should "completely parse" in {
     val objParser = new GCodeParser
-    val arg = "n0g0  x1 y2  z3 (comme\nnt comment)\nN1 g1 x2 y2 z3"
-    val arg1 = objParser.filterOutWhiteSpace(arg)
-    val parsed = objParser.parse(objParser.gcodeCommands,arg1)
+    val arg = "(comment)G17 X0 i2j-1(comment)"
+    val parsed = objParser.parse(objParser.gSingleLineCommand,objParser.filterOutWhiteSpace(arg))
+    val ret = parsed.get match { case rv:GcodeSingleCommand => rv }
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
-    parsed.successful should be (true)
-    val ret = parsed.get
     rest.size should be (0)
-    ret.commands.size should be (2)
     
-    var command = ret.commands(0)
-    var steps = command.steps
-    var stepData = steps(0).stepData.toArray
-    command.key should be ("G0")    
-    steps.size should be (1)
+    val parameters = ret.parameters.get
+    parameters.parameters.size should be (3)
+    val stepData = parameters.parameters.toArray
+    ret.key should be ("G17")
     stepData.size should be (3)
     stepData(0).key   should be ("X")
-    stepData(0).value should be (1f)
-    stepData(1).key   should be ("Y")
+    stepData(0).value should be (0f)
+    stepData(1).key   should be ("I")
     stepData(1).value should be (2f)
-    stepData(2).key   should be ("Z")
-    stepData(2).value should be (3f)
-    
-    command = ret.commands(1)
-    steps = command.steps
-    stepData = steps(0).stepData.toArray
-    
-    stepData.size should be (3)
-    steps.size should be (1)
-    stepData(0).key   should be ("X")
-    stepData(0).value should be (2f)
-    stepData(1).key   should be ("Y")
-    stepData(1).value should be (2f)
-    stepData(2).key   should be ("Z")
-    stepData(2).value should be (3f)
+    stepData(2).key   should be ("J")
+    stepData(2).value should be (-1f)
   }
   
-  "multigcode multiline with comment 2" should "completely parse" in {
+  "gSingleLineCommand with G0" should "completely fail" in {
+    val objParser = new GCodeParser
+    val arg = "(comment)G0 X0 i2j-1(comment)"
+    val parsed = objParser.parse(objParser.gSingleLineCommand,objParser.filterOutWhiteSpace(arg))
+    parsed.successful should be (false)
+  }
+  
+  "gSingleLineCommand with G00" should "completely fail" in {
+    val objParser = new GCodeParser
+    val arg = "(comment)G0 X0 i2j-1(comment)"
+    val parsed = objParser.parse(objParser.gSingleLineCommand,objParser.filterOutWhiteSpace(arg))
+    parsed.successful should be (false)
+  }
+  
+  "gSingleLineCommand with G1" should "completely fail" in {
+    val objParser = new GCodeParser
+    val arg = "(comment)G1 X0 i2j-1(comment)"
+    val parsed = objParser.parse(objParser.gSingleLineCommand,objParser.filterOutWhiteSpace(arg))
+    parsed.successful should be (false)
+  }
+  
+  "gSingleLineCommand with G01" should "completely fail" in {
+    val objParser = new GCodeParser
+    val arg = "(comment)G1 X0 i2j-1(comment)"
+    val parsed = objParser.parse(objParser.gSingleLineCommand,objParser.filterOutWhiteSpace(arg))
+    parsed.successful should be (false)
+  }
+   
+  "gCode multiline with comment 2" should "completely parse" in {
     val objParser = new GCodeParser
     val arg = "    G17 G20 G40 G49 G54 G80 G90 G94\nN 0 g0  x1 y2  z3 (comme\nnt comment)\nx1 y1 z1\nN1 g1 x2 y2 z3"
-    val parsed = objParser.parse(objParser.gcodeCommands,objParser.filterOutWhiteSpace(arg))
+    val parsed = objParser.parse(objParser.gCode,objParser.filterOutWhiteSpace(arg))
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
     parsed.successful should be (true)
     
     val ret = parsed.get
     rest.size should be (0)
-
-    ret.commands.size should be (2)
+    ret.commands.size should be (10)
     
-    var command = ret.commands(0)
-    var steps = command.steps
-    var stepData = steps(0).stepData.toArray
-    command.key should be ("G0")    
-    steps.size should be (2)
-    stepData.size should be (3)
+    var command = ret.commands(0) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G17")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(1) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G20")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(2) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G40")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(3) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G49")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(4) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G54")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(5) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G80")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(6) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G90")   
+    command.parameters.size should be (0)
+    
+    command = ret.commands(7) match { case c:GcodeSingleCommand => c}
+    command.key should be ("G94")   
+    command.parameters.size should be (0)
+    
+    var command1 = ret.commands(8) match { case c:GcodeMultiCommand => c}
+    command1.key should be ("G0")   
+    command1.steps.size should be (2)
+    var stepData = command1.steps(0).parameters
     stepData(0).key   should be ("X")
     stepData(0).value should be (1f)
     stepData(1).key   should be ("Y")
@@ -194,30 +234,20 @@ class GCodeParserTests1 extends FlatSpec with Matchers {
     stepData(2).key   should be ("Z")
     stepData(2).value should be (3f)
     
-    stepData = steps(1).stepData.toArray
+    command1 = ret.commands(9) match { case c:GcodeMultiCommand => c}
+    command1.key should be ("G1")   
+    command1.steps.size should be (1)
+    stepData = command1.steps(0).parameters
     stepData.size should be (3)
-    stepData(0).key   should be ("X")
-    stepData(0).value should be (1f)
-    stepData(1).key   should be ("Y")
-    stepData(1).value should be (1f)
-    stepData(2).key   should be ("Z")
-    stepData(2).value should be (1f)
-    
-    command = ret.commands(1)
-    steps = command.steps
-    stepData = steps(0).stepData.toArray
-    
-    stepData.size should be (3)
-    steps.size should be (1)
     stepData(0).key   should be ("X")
     stepData(0).value should be (2f)
     stepData(1).key   should be ("Y")
     stepData(1).value should be (2f)
     stepData(2).key   should be ("Z")
-    stepData(2).value should be (3f)
+    stepData(2).value should be (3f) 
   }
-
-  "multigcode multiline with comment 3" should "completely parse" in {
+ 
+  "gCode multiline with comment 3" should "completely parse" in {
     val objParser = new GCodeParser
     val arg = """
   G0Z2.0
@@ -231,12 +261,14 @@ G0Z2.000000
 G0X19.01426Y26.41212
 G1X19.01426Y26.41212Z0F500.000000
 G1X19.01426Y26.41212Z-11.68791F100.000000
-G1X19.01426Y26.41212Z-11.68791F500.000000
+X19.01426Y26.41212Z-11.68791F500.000000
 G1X19.01426Y26.41212Z-11.68791"""
-    val parsed = objParser.parse(objParser.gcodeCommands,objParser.filterOutWhiteSpace(arg))
+    val parsed = objParser.parse(objParser.gCode,objParser.filterOutWhiteSpace(arg))
     val rest = parsed.next.source.toString.substring(parsed.next.offset)
     if (rest.size > 0) println("unparsed:" + rest)
     parsed.successful should be (true)
     rest.size should be (0)
+    val ret = parsed.get
+    ret.commands.size should be (11)
   }  
 }
