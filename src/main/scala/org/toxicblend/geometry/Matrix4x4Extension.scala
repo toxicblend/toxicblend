e530bc45415750e0b1eb2f37adb8bd78279578f1
EAD Fritz <ead.fritz@gmail.com>
2014-02-21 21:19:17 +0100

    Added a translateScale method

--- src/main/scala/org/toxicblend/geometry/Matrix4x4Extension.scala
+++ src/main/scala/org/toxicblend/geometry/Matrix4x4Extension.scala
@@ -1,5 +1,6 @@
 package org.toxicblend.geometry
 import toxi.geom.Matrix4x4
+import toxi.geom.ReadonlyVec3D
 
 class Matrix4x4Extension( __extended:Matrix4x4) extends Matrix4x4(__extended) {
   
@@ -17,6 +18,18 @@ class Matrix4x4Extension( __extended:Matrix4x4) extends Matrix4x4(__extended) {
         }
       })
     })
+    this
+  }
+  
+  /**
+   * converts this matrix into a scale and translate matrix (in place)
+   */
+  def translateScale(translate:ReadonlyVec3D, scale:ReadonlyVec3D):Matrix4x4Extension = {
+    this.set( scale.x(),         0,         0, translate.x(), 
+                      0, scale.y(),         0, translate.y(),
+                      0,         0, scale.z(), translate.z(),
+                      0,         0,         0,             1)
+            
     this
   }
 }
