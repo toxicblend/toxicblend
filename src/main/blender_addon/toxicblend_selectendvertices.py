24a7e5cad71de9b3561f882f4359e58bec85231d
EAD Fritz <ead.fritz@gmail.com>
2014-02-26 14:59:23 +0100

    Removed the edit<->object mode switching Also made it a proper addon with a menu location and all (Mesh->Select)

--- src/main/blender_addon/toxicblend_selectendvertices.py
+++ src/main/blender_addon/toxicblend_selectendvertices.py
@@ -1,12 +1,14 @@
 import bpy
 import bmesh
+import array
 
 bl_info = {
   'name': "Toxicblend - Select end vertices (standalone)",
   'description': 'Selects all vertices that are only connected to one other vertice (standalone)',
   'author': 'EAD Fritz',
   'blender': (2, 69, 0),
-  "category": "3D View",
+  'category': 'Mesh',
+  'location': 'View3D > EditMode > Select',
 }
 
 class ToxicBlend_SelectEndVertices(bpy.types.Operator):
@@ -20,33 +22,44 @@ class ToxicBlend_SelectEndVertices(bpy.types.Operator):
     return context.active_object is not None
 
   def execute(self, context):
-    bm = bmesh.new()
-    me = bpy.context.active_object.data
-    bm.from_mesh(me)
+      
+    # Get the active mesh
+    obj = bpy.context.edit_object
+    me = obj.data
+
+    # Get a BMesh representation
+    bm = bmesh.from_edit_mesh(me)
+    bpy.ops.mesh.select_all(action='DESELECT')   
+        
     if len(bm.edges) > 0 or len(bm.faces) > 0:
-      verticeConnections = [0 for i in me.vertices]
-      for e in me.edges:
-        for vi in e.vertices:
-          verticeConnections[vi] += 1
+      verticeConnections = array.array('i',(0 for i in range(0,len(bm.verts))))
+      for e in bm.edges:
+        for vi in e.verts:
+          verticeConnections[vi.index] += 1
       for f in bm.faces:
-        for vi in f.vertices:
-          verticeConnections[vi] += 1  
-          
-      # do a little dance to make sure that the vertex new selection is visible                
-      bpy.ops.object.mode_set(mode='EDIT')
-      bpy.ops.mesh.select_all(action='DESELECT')
-      bpy.ops.object.mode_set(mode='OBJECT')     
+        for vi in f.verts:
+          verticeConnections[vi.index] += 1  
+            
       for vi in range(0,len(verticeConnections)):
         if (verticeConnections[vi] < 2):
-          me.vertices[vi].select = True     
-      bpy.ops.object.mode_set(mode='EDIT') 
+          bm.verts[vi].select = True     
+   
+    # Show the updates in the viewport
+    bmesh.update_edit_mesh(me, False)
+
     return {'FINISHED'}
 
+def menu_func(self, context):
+  self.layout.operator(ToxicBlend_SelectEndVertices.bl_idname, text=ToxicBlend_SelectEndVertices.bl_label)
+    
 def register():
-  bpy.utils.register_class(ToxicBlend_SelectEndVertices)
-
+  bpy.utils.register_module(__name__)
+  bpy.types.VIEW3D_MT_select_edit_mesh.append(menu_func)
+ 
 def unregister():
-  bpy.utils.unregister_class(ToxicBlend_SelectEndVertices)
-
+  bpy.utils.unregister_module(__name__)
+  bpy.types.VIEW3D_MT_select_edit_mesh.remove(menu_func)
+    
 if __name__ == "__main__":
+  unregister()
   register()    
