297db05f8192b6068559363f8861e83bf886ba0d
EAD Fritz <ead.fritz@gmail.com>
2014-03-01 11:52:29 +0100

    more tests

--- src/test/scala/org/toxicblend/tests/JBulletTest2.scala
+++ src/test/scala/org/toxicblend/tests/JBulletTest2.scala
@@ -15,53 +15,88 @@ class JBulletTest2 extends FlatSpec with Matchers {
   
   val models = {
     val toximesh = new TriangleMesh
-    toximesh.addFace(new Vec3D(1,-1,0), new Vec3D(-1,-1,0), new Vec3D(-1,1,0))
-    toximesh.addFace(new Vec3D(-1,1,0), new Vec3D(1,1,0), new Vec3D(1,-1,0))
+    toximesh.addFace(new Vec3D( 2,-2,0), new Vec3D(-2,-2,0), new Vec3D(-2, 2,0))
+    toximesh.addFace(new Vec3D(-2, 2,0), new Vec3D( 2, 2,0), new Vec3D( 2,-2,0))
     val model = Mesh3DConverter(toximesh,"JBulletTest2")
     Array(model)
   }
   
-  val segments:Array[IndexedSeq[ReadonlyVec3D]] = {
-    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(0f,-.7071068f,1f), 
-                                             new Vec3D(-.7071068f,0f,1f),
-                                             new Vec3D(.7071068f,0f,1f),
-                                             new Vec3D(.7071068f,0f,1f))
-    Array(segment)
-  }
-  val jbc = new JBulletCollision(segments, models, 0.005f, 0.0001f)
-  
-  "jbullet2" should "collide just fine" in {
-           
+  def doTheRayTests(segment:Array[ReadonlyVec3D]) = {
     val result = new MutableList[IndexedSeq[ReadonlyVec3D]]
-    segments.foreach(segment => {
-      segment.sliding(2,1).foreach(s => {
-        val r = jbc.doRayTests(s)
-        //println("from: " + s(0) + " to:" + s(1) + " = " + r.mkString("\n   ","\n   ", ""))
-        result += r.flatten
-      })
+    segment.sliding(2,1).foreach(s => {
+      val r = jbc.doRayTests(s)
+      //println("from: " + s(0) + " to:" + s(1) + " = " + r.mkString("\n   ","\n   ", ""))
+      result += r.flatten
     })
-    println("Segment: " + segments(0))
-    println("Result: " + result.mkString("\n"))
-    //result.size should be (4)
+    result
+  }
+  
+  val jbc = new JBulletCollision(models, 0.005f, 0.0001f)
+  "jbullet2-1" should "collide just fine" in {    
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(0f,-1f,1f), 
+                                             new Vec3D(-1f,0f,1f),
+                                             new Vec3D(0f,1f,1f),
+                                             new Vec3D(1f,0f,1f),
+                                             new Vec3D(0f,-1f,1f))                                       
+    val result = doTheRayTests(segment)
+    //println("input Segment: " + segment )
+    //println("Result segments:\n" + result.mkString("\n"))
     val first = result(0)(0)
     val last = result.last.last
-    //val firstInput = segments(0)(0)
-    //val LastInput = segments(0).last
     last.x should be (first.x)
     last.y should be (first.y)
     last.z should be (first.z)
-    
-    /*val subResult = result(0)
-    subResult.size should be (4)
-    subResult(0).x should be (segments(0)(0).x)
-    subResult(0).y should be (segments(0)(0).y)
-    subResult.last.x should be (segments(0).last.x)
-    subResult.last.y should be (segments(0).last.y)
-    *///
+  }
+  
+  "jbullet2-2" should "collide just fine" in {       
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(1f,0f,1f),
+                                             new Vec3D(0f,-1f,1f), 
+                                             new Vec3D(-1f,0f,1f),
+                                             new Vec3D(0f,1f,1f),
+                                             new Vec3D(1f,0f,1f))      
+    val result = doTheRayTests(segment)
+    //println("input Segment: " + segment )
+    //println("Result segments:\n" + result.mkString("\n"))
+    val first = result(0)(0)
+    val last = result.last.last
+    last.x should be (first.x)
+    last.y should be (first.y)
+    last.z should be (first.z)
+  }
+  
+  "jbullet2-3" should "collide just fine" in {  
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(-1f,0f,1f),
+                                             new Vec3D(0f,1f,1f),
+                                             new Vec3D(1f,0f,1f),
+                                             new Vec3D(0f,-1f,1f),
+                                             new Vec3D(-1f,0f,1f))                                 
+    val result = doTheRayTests(segment)
+    //println("input Segment: " + segment )
+    //println("Result segments:\n" + result.mkString("\n"))
+    val first = result(0)(0)
+    val last = result.last.last
+    last.x should be (first.x)
+    last.y should be (first.y)
+    last.z should be (first.z)
+  }
+  
+  "jbullet2-4" should "collide just fine" in {  
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(-1f,0f,1f),
+                                             new Vec3D(0f,-1f,1f),
+                                             new Vec3D(1f,0f,1f), 
+                                             new Vec3D(0f,1f,1f),
+                                             new Vec3D(-1f,0f,1f))                                 
+    val result = doTheRayTests(segment)
+    //println("input Segment: " + segment.mkString("\n") )
+    //println("Result segments:\n" + result.mkString("\n"))
+    val first = result(0)(0)
+    val last = result.last.last
+    last.x should be (first.x)
+    last.y should be (first.y)
+    last.z should be (first.z)
   }
   
   "jbullet cleanup" should "not fail" in {
     jbc.cleanup
   }
-  
 }
