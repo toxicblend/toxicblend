6f5a481847c2f04edc753b950916f3573a1c3a3b
EAD Fritz <ead.fritz@gmail.com>
2014-03-02 19:44:59 +0100

    Added more tests, some failing tests too - still looking for that bug

--- src/test/scala/org/toxicblend/tests/JBulletTest1.scala
+++ src/test/scala/org/toxicblend/tests/JBulletTest1.scala
@@ -10,40 +10,90 @@ import org.scalatest._
 
 
 class JBulletTest1 extends FlatSpec with Matchers {
-  
+  val epsilon = 0.00001f
+  val triangle = Array(new Vec3D(0,0,1), new Vec3D(1,0,1), new Vec3D(0,1,1))
   val models = {
     val toximesh = new TriangleMesh
-    toximesh.addFace(new Vec3D(0,0,1), new Vec3D(1,0,1), new Vec3D(0,1,1))
+    toximesh.addFace(triangle(0), triangle(1), triangle(2))
     val model = Mesh3DConverter(toximesh,"JBulletTest1")
     Array(model)
   }
   
-  val segments:Array[IndexedSeq[ReadonlyVec3D]] = {
-    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(-2f,-2f,1f), new Vec3D(2f,2f,1f))
-    Array(segment)
-  }
   val jbc = new JBulletCollision(models, 0.005f, 0.0001f)
   
+  def testCollider(segment:Array[ReadonlyVec3D]) = {
+    val result = new MutableList[IndexedSeq[ReadonlyVec3D]]
+    segment.sliding(2,1).foreach(s => {
+      val r = jbc.doCollisionTests(s)
+      //println("from: " + s(0) + " to:" + s(1) + " = " + r.mkString("\n   ","\n   ", ""))
+      result += r.flatten
+    })
+    result
+  }
   
   "jbullet1" should "collide just fine" in {
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(-2f,-2f,2f), new Vec3D(2f,2f,2f))      
+    val result = testCollider(segment)
+    println("Result: " + result)
+    result.size should be (1)
+    val subResult = result(0)
+    subResult.size should be (6)
+    subResult(0).x should be ((segment(0).x) +- epsilon)
+    subResult(0).y should be ((segment(0).y) +- epsilon)
+    subResult(0).z should be ((0f) +- epsilon)
+    
+    subResult(1).x should be ((triangle(0).x) +- epsilon)
+    subResult(1).y should be ((triangle(0).y) +- epsilon)
+    subResult(1).z should be ((0f) +- epsilon)
+    
+    subResult(2).x should be ((triangle(0).x) +- epsilon)
+    subResult(2).y should be ((triangle(0).y) +- epsilon)
+    subResult(2).z should be ((triangle(0).z) +- epsilon)
+    
+    subResult(3).x should be ((.5f) +- epsilon)
+    subResult(3).y should be ((.5f) +- epsilon)
+    subResult(3).z should be ((1f) +- epsilon)
+    
+    subResult(4).x should be ((.5f) +- epsilon)
+    subResult(4).y should be ((.5f) +- epsilon)
+    subResult(4).z should be ((0f) +- epsilon)
+    
+    subResult.last.x should be ((segment.last.x) +- epsilon)
+    subResult.last.y should be ((segment.last.y) +- epsilon)
+    //
+  }
+  
+  "jbullet2" should "collide along the edge just fine" in {
            
-    val result = new MutableList[IndexedSeq[ReadonlyVec3D]]
-    segments.foreach(segment => {
-      segment.sliding(2,1).foreach(s => {
-        val r = jbc.doRayTests(s)
-        //println("from: " + s(0) + " to:" + s(1) + " = " + r.mkString("\n   ","\n   ", ""))
-        result += r.flatten
-      })
-    })
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(0f,-1f,2f), new Vec3D(0f,2f,2f))      
+    val result = testCollider(segment)
+    
     //println("Result: " + result)
     result.size should be (1)
     val subResult = result(0)
     subResult.size should be (6)
-    subResult(0).x should be (segments(0)(0).x)
-    subResult(0).y should be (segments(0)(0).y)
-    subResult.last.x should be (segments(0).last.x)
-    subResult.last.y should be (segments(0).last.y)
-    //
+    subResult(0).x should be ((segment(0).x) +- epsilon)
+    subResult(0).y should be ((segment(0).y) +- epsilon)
+    
+    subResult(1).x should be ((triangle(0).x) +- epsilon)
+    subResult(1).y should be ((triangle(0).y) +- epsilon)
+    subResult(1).z should be ((0f) +- epsilon)
+    
+    subResult(2).x should be ((triangle(0).x) +- epsilon)
+    subResult(2).y should be ((triangle(0).y) +- epsilon)
+    subResult(2).z should be ((triangle(0).z) +- epsilon)
+    
+    subResult(3).x should be ((triangle(2).x) +- epsilon)
+    subResult(3).y should be ((triangle(2).y) +- epsilon)
+    subResult(3).z should be ((triangle(2).z) +- epsilon)
+    
+    subResult(4).x should be ((triangle(2).x) +- epsilon)
+    subResult(4).y should be ((triangle(2).y) +- epsilon)
+    subResult(4).z should be ((0f) +- epsilon)
+    
+    subResult.last.x should be ((segment.last.x)+- epsilon)
+    subResult.last.y should be ((segment.last.y)+- epsilon)
+
   }
   
   "jbullet cleanup" should "not fail" in {
--- src/test/scala/org/toxicblend/tests/JBulletTest2.scala
+++ src/test/scala/org/toxicblend/tests/JBulletTest2.scala
@@ -7,6 +7,7 @@ import org.toxicblend.typeconverters.Mesh3DConverter
 import scala.collection.mutable.MutableList
 import org.toxicblend.operations.zadjust.jbullet.JBulletCollision
 import org.scalatest._
+import matchers.ShouldMatchers._
 
 /**
  * this is a failed test i noticed while playing around in blender
@@ -24,7 +25,7 @@ class JBulletTest2 extends FlatSpec with Matchers {
   def doTheRayTests(segment:Array[ReadonlyVec3D]) = {
     val result = new MutableList[IndexedSeq[ReadonlyVec3D]]
     segment.sliding(2,1).foreach(s => {
-      val r = jbc.doRayTests(s)
+      val r = jbc.doCollisionTests(s)
       //println("from: " + s(0) + " to:" + s(1) + " = " + r.mkString("\n   ","\n   ", ""))
       result += r.flatten
     })
@@ -94,6 +95,32 @@ class JBulletTest2 extends FlatSpec with Matchers {
     last.x should be (first.x)
     last.y should be (first.y)
     last.z should be (first.z)
+  }
+  
+  "jbullet-edge-1" should "collide just fine" in {  
+    val segment:Array[ReadonlyVec3D] = Array(new Vec3D(-1f,-1f,1f),
+                                             new Vec3D(0f,0f,1f),
+                                             new Vec3D(1f,1f,1f))                                 
+    val result = doTheRayTests(segment)
+    println("input Segment: " + segment.mkString("\n") )
+    println("Result segments:\n" + result.mkString("\n"))
+    val firstI = segment(0)
+    val lastI = segment.last
+    val firstO = result(0)(0)
+    val lastO = result.last.last
+    println("firstI: " +firstI + " firstO=" +firstO)
+    println("lastI: " +lastI + " lastO=" +lastO)
+    
+    result.size should be (2)
+     
+    firstO.x should be (firstI.x)
+    firstO.y should be (firstI.y)
+    firstO.z should be ((0f) plusOrMinus 1e-5f) 
+    
+    lastO.x should be (lastI.x)
+    lastO.y should be (lastI.y)
+    lastO.z should be ((0f) plusOrMinus 1e-5f)
+    
   }
   
   "jbullet cleanup" should "not fail" in {
