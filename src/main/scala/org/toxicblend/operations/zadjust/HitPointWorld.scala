b22fc5cfef233863b2b4279d7a1f159b90a41c1e
EAD Fritz <ead.fritz@gmail.com>
2014-03-05 22:20:03 +0100

    added toString

--- src/main/scala/org/toxicblend/operations/zadjust/HitPointWorld.scala
+++ src/main/scala/org/toxicblend/operations/zadjust/HitPointWorld.scala
@@ -5,4 +5,9 @@ class HitPointWorld {
   val point = new Point3dE
   var triangleIndex:Int = -1
   def copyHitpoint = new Point3dE(point)
+  @inline def hasTriangleIndex=triangleIndex>=0
+  
+  override def toString() = {
+    "point:" + point + " triangleIndex:" + triangleIndex 
+  } 
 }
