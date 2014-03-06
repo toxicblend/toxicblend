0212e8608c7faa944970787f22aa03f7ad6f1ecf
EAD Fritz <ead.fritz@gmail.com>
2014-03-06 19:31:22 +0100

    Reworked the triangle-plane detection

--- src/main/scala/org/toxicblend/operations/zadjust/Collider.scala
+++ src/main/scala/org/toxicblend/operations/zadjust/Collider.scala
@@ -11,6 +11,9 @@ import toxi.geom.ReadonlyVec3D
 import com.bulletphysics.linearmath.Vector3dE
 import com.bulletphysics.linearmath.Point3dE
 import com.bulletphysics.linearmath.VectorUtil
+import com.bulletphysics.linearmath.Triangle
+import com.bulletphysics.linearmath.Line3d
+import com.bulletphysics.linearmath.Plane
 
 /**
  * This is a 'per thread' object, everything that pertain to the state of a specific calculation thread should be stored here.
@@ -18,22 +21,22 @@ import com.bulletphysics.linearmath.VectorUtil
  */
 class Collider(val facade:BulletFacade, val sampleStep:Double, val epsilon:Double) {
   
-  val direction = new Vector3dE
   val directionIncrement = new Vector3dE
-  val samplePoint = new Point3dE
   var distanceToCoverSqr = 0d
   var distanceCoveredSqr = 0d
+  val triangle = new Triangle
+  val currentPos = new Line3d
+  val currentPlane = new Plane
+  val triangleData = Array(0,0,0)
+  val zMin = facade.aabbAllModels.getMin.z-1
+  val zMax = facade.aabbAllModels.getMax.z+1
   
   val convexCallback = {
-    val aabb = facade.aabbAllModels
     val convexShape = facade.coneShapeZ
-    new ClosestConvexResultCallback(convexShape.rotation,convexShape.zAdjust,aabb.getMin.z-1, aabb.getMax.z+1)
+    new ClosestConvexResultCallback(convexShape.rotation, convexShape.zAdjust, zMin, zMax)
   }
   
-  val rayCallback = {
-    val aabb = facade.aabbAllModels
-    new ClosestRayResultCallback(aabb.getMin.z-1, aabb.getMax.z+1)
-  }
+  val rayCallback = new ClosestRayResultCallback(zMin, zMax)
   
   /**
    * sample point 
@@ -61,43 +64,92 @@ class Collider(val facade:BulletFacade, val sampleStep:Double, val epsilon:Doubl
    *  end of world samples where the ray misses the 'ground'
    *  end point (toP)  
    */
-  def collisionTestSegment(fromP:Point3dE, toP:Point3dE, sumZ:Boolean):IndexedSeq[Point3dE] = {
-     @inline
-     def adjustSample(sample:Tuple3d, interpolated:Tuple3d) = {
-       sample.z = sample.z + interpolated.z
-     }
-     
+  def collisionTestSegment(fromP:Point3dE, toP:Point3dE, sumInputAndOutputZ:Boolean):IndexedSeq[Point3dE] = {
+    @inline
+    def adjustSample(sample:Tuple3d, interpolated:Tuple3d) = {
+      sample.z = sample.z + interpolated.z
+    }
+    currentPos.setSelf(fromP,toP)
+    currentPos.getZPlane(currentPlane)
+    
     val rv = new ArrayBuffer[Point3dE]
-    direction.setSelf(toP).subSelf(fromP)
-    directionIncrement.setSelf(direction).normalizeSelf.scaleSelf(sampleStep)
-    samplePoint.setSelf(fromP)
+    
+    directionIncrement.setSelf(currentPos.dir).normalizeSelf.scaleSelf(sampleStep)
     distanceCoveredSqr = 0d
-    distanceToCoverSqr = fromP.xyDistance(toP)
+    distanceToCoverSqr = fromP.xyDistanceSqr(toP)
     val distanceToCover = math.sqrt(distanceToCoverSqr)
+    val interSectionResult = new Plane.IntersectionResult
+    //println("New segment" + fromP + " toP: " + toP)
     do {
-      rv += collisionTestPoint(samplePoint).copyHitpoint
-      samplePoint.addSelf(directionIncrement)
-      distanceCoveredSqr = fromP.xyDistance(samplePoint)
+      val cp = collisionTestPoint(currentPos.origin)
+      if (cp.hasTriangleIndex) {
+        facade.models(0).readTriangle(triangle, cp.triangleIndex)
+        currentPlane.getZIntersectionWithTriangle(triangle,currentPos,interSectionResult)
+      }
+      if (cp.hasTriangleIndex && interSectionResult.hasResult) {
+        // we hit something    
+        val prevEdgePoint = interSectionResult.prevPoint
+        val nextEdgePoint = interSectionResult.nextPoint
+        
+        if (rv.size==0){
+          rv += cp.copyHitpoint  
+        } else if( rv.last.z == zMin) {
+          rv += new Point3dE(prevEdgePoint); rv.last.z = zMin
+          rv += new Point3dE(prevEdgePoint);
+        }
+        val dotP = new Vector3dE(nextEdgePoint).subSelf(fromP).normalizeSelf.xyDot(new Vector3dE(currentPos.dir).normalizeSelf)
+        if (dotP < 0) {
+           println("negative dotP: triangleidx:" + cp.triangleIndex + " nextEdgePoint: " + nextEdgePoint + " dotP=" + dotP + " prevPoint=" + interSectionResult.prevPoint )
+           println("dir: "+ currentPos.dir )
+        }
+        val distanceSqr = fromP.xyDistanceSqr(nextEdgePoint)
+
+        if (distanceSqr > distanceCoveredSqr && distanceSqr < distanceToCoverSqr && dotP > 0) {
+          //println("jumping ahead: " + nextEdgePoint + " distanceSqr: " + distanceSqr + " distanceCoveredSqr:" + distanceCoveredSqr+ " dotP:" + dotP)
+          currentPos.origin.setSelf(nextEdgePoint)
+          rv += new Point3dE(nextEdgePoint)
+        } else if (distanceSqr > distanceCoveredSqr && dotP > 0){
+          // just set the overshoot position, it will be adjusted later
+          currentPos.origin.setSelf(nextEdgePoint)
+        } else {
+          currentPos.origin.addSelf(directionIncrement)
+          if(distanceSqr!=distanceCoveredSqr) {
+            System.err.println("nextEdgePoint would set me back:" + nextEdgePoint + " distanceSqr: " + distanceSqr + " distanceCoveredSqr:" + distanceCoveredSqr + " dotP:" + dotP)
+          }
+        }
+      } else {
+        // we hit nothing
+        if (rv.size>0 && rv.last.z == zMin && cp.point.z == zMin){
+          // no need to store another air sample
+        } else {
+          rv += cp.copyHitpoint
+        }
+        currentPos.origin.addSelf(directionIncrement)
+      }
+      distanceCoveredSqr = fromP.xyDistanceSqr(currentPos.origin)
+      //println("" + currentPos + " distanceCoveredSqr:" + distanceCoveredSqr + " distanceToCoverSqr:" + distanceToCoverSqr)
+      
+      // adjust the position if it overshot the target
       if (distanceCoveredSqr > distanceToCoverSqr){
-        samplePoint.set(toP)
-        rv += collisionTestPoint(samplePoint).copyHitpoint
+        currentPos.origin.set(toP)
+        // What about the Z coordinate??? this can't be correct
+        rv += collisionTestPoint(currentPos.origin).copyHitpoint
         distanceCoveredSqr = distanceToCoverSqr
       }
     } while (distanceCoveredSqr < distanceToCoverSqr)  
       
-    if (sumZ) {
+    if (sumInputAndOutputZ) {
       // Interpolate the Z values on a line between fromP and toP
-      adjustSample(rv.head, fromP)
       val interpolated = new Vector3dE
-      // iterator.sliding(2).map(_.head) == all but last
-      //rv.tail.iterator.sliding(2).map(_.head).foreach(sample => {  
+      adjustSample(rv.head, fromP)
+      // iterate over each 'in the middle' point and interpolate the result
       (1 until rv.size -1).foreach(i => {
         val sample = rv(i)
         val ratio = fromP.xyDistance(sample)/distanceToCover
         VectorUtil.setInterpolate3(interpolated, fromP, toP, ratio)
         adjustSample(sample, interpolated)
       })
-      // Don't interpolate the last position, it gives jagged edges. Just use the real thing
+      // Don't interpolate the last position, it gives jagged edges. Just use the real coordinate
       adjustSample(rv.last, toP)
     }
     rv
@@ -105,7 +157,14 @@ class Collider(val facade:BulletFacade, val sampleStep:Double, val epsilon:Doubl
   
   def doCollisionTests(segment:IndexedSeq[Point3dE], sumZ:Boolean) : IndexedSeq[Point3dE]= {
     val rv = new ArrayBuffer[Point3dE]
-    segment.sliding(2).foreach(s => rv ++= collisionTestSegment(s(0), s(1), sumZ))
+    segment.sliding(2).foreach(s => {
+      // temporary test
+      if (s(0).x==s(1).x && s(0).y==s(1).y && s(0).z==s(1).z)
+        println("ignoring test of " + s(0) + " and " + s(1) )
+      else {
+        rv ++= collisionTestSegment(s(0), s(1), sumZ)
+      }
+    })
     rv
   }
 }
