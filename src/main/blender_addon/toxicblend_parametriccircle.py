c24635f8a175d87bf260602356995907ef2076de
EAD Fritz <ead.fritz@gmail.com>
2014-03-10 22:15:30 +0100

    Added cursor position

--- src/main/blender_addon/toxicblend_parametriccircle.py
+++ src/main/blender_addon/toxicblend_parametriccircle.py
@@ -17,15 +17,16 @@ class ToxicBlend_ParametricCircle(bpy.types.Operator):
   bl_label = "Toxicblend:Custom draw circle"
   bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
   
-  projectionPlaneProperty = bpy.props.EnumProperty(
-    name="Choose 2D plane projection",
-    description = "For now manual projection selection will be used.",
-    items=(("YZ_PLANE", "YZ",""),
-           ("XZ_PLANE", "XZ",""), 
-           ("XY_PLANE", "XY","")),
+  drawTypeProperty = bpy.props.EnumProperty(
+    name="Draw type",
+    description = "Select draw type.",
+    items=(("CIRCLE", "CIRCLE",""),
+           ("BOX", "BOX",""), 
+           ("SPHERE", "SPHERE","")),
            #update=mode_update_callback
-           default="XY_PLANE"    
+           default="CIRCLE"    
           )
+            
   useMultiThreadingProperty = bpy.props.EnumProperty(
     description="Each continous ring segment will be processed in a separate thread",
     name="Use mulithreading algorithm",
@@ -35,25 +36,29 @@ class ToxicBlend_ParametricCircle(bpy.types.Operator):
            default="FALSE"    
           )
           
-  simplifyLimitProperty = bpy.props.FloatProperty(name="Simplify Limit (Not used yet)", default=0.5, min=0.0001, max=100, description="the maximum allowed 3d deviation (in pixels) from a straight line, if the deviation is larger than this the line will be segmented.")  
+  simplifyLimitProperty = bpy.props.FloatProperty(name="Simplify Limit (Not used yet)", default=0.5, min=0.0001, max=100, description="the maximum allowed 3d deviation (in pixels) from a straight line, if the deviation is larger than this the line will be segmented.")       
   zEpsilonProperty = bpy.props.FloatProperty(name="z Epsilon", description="Z values smaller than this is considered to be zero, these points enables 'dot product limit'", default=1.5, min=0.00001, max=10)
+    
   dotProductLimitProperty = bpy.props.FloatProperty(name="Dot Product Limit", description="filter for internal edges relative to the outer ring segment, ideally only edges with 90 degree angles should be kept", default=0.95, min=0.0001, max=1)
-  calculationResolutionProperty = 46338 # sqrt(Int.MaxValue)-2
-
+  
   def execute(self, context):
     imp.reload(toxicblend)
     with toxicblend.ByteCommunicator("localhost", 9999) as c: 
       # bpy.context.selected_objects,
       unitSystemProperty = context.scene.unit_settings
       activeObject = context.scene.objects.active
-      properties = {'projectionPlane'       : str(self.projectionPlaneProperty), 
+      
+      cursor_location = bpy.context.scene.cursor_location.copy()
+      properties = {'drawTypeProperty'      : str(self.drawTypeProperty), 
                     'useMultiThreading'     : str(self.useMultiThreadingProperty),
                     'simplifyLimit'         : str(self.simplifyLimitProperty),
                     'zEpsilon'              : str(self.zEpsilonProperty),
                     'dotProductLimit'       : str(self.dotProductLimitProperty),
-                    'calculationResolution' : str(self.calculationResolutionProperty),
                     'unitSystem'            : str(unitSystemProperty.system), 
-                    'unitScale'             : str(unitSystemProperty.scale_length) }
+                    'unitScale'             : str(unitSystemProperty.scale_length),
+                    'cursorPosX'            : str(cursor_location.x),
+                    'cursorPosY'            : str(cursor_location.y),
+                    'cursorPosZ'            : str(cursor_location.z)}
                      
       c.sendOnlyCommand(self.bl_idname, properties) 
       c.receiveObjects()
