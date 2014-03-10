caf8bbd45686835d166046eeb5715228eedea8fe
EAD Fritz <ead.fritz@gmail.com>
2014-03-10 22:14:03 +0100

    Numbers can now be negative as well :)

--- src/main/scala/org/toxicblend/util/Regex.scala
+++ src/main/scala/org/toxicblend/util/Regex.scala
@@ -1,6 +1,6 @@
 package org.toxicblend.util
 
 object Regex {
-  lazy val FLOAT_REGEX =  """(\d*(?:\.\d*)?(?:[E|e]-?\d+)?)""".r
-  lazy val INT_REGEX =  """(\d+)""".r
+  lazy val FLOAT_REGEX =  """(-?\d*(?:\.\d*)?(?:[E|e]-?\d+)?)""".r
+  lazy val INT_REGEX =  """(-?\d+)""".r
 }
