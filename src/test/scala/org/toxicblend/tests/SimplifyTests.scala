c5ce5e5ae3f5f9a9bad1e441949f8d7eb40e3694
EAD Fritz <ead.fritz@gmail.com>
2014-04-05 18:52:10 +0200

    Added linesegment distance tests

--- src/test/scala/org/toxicblend/tests/SimplifyTests.scala
+++ src/test/scala/org/toxicblend/tests/SimplifyTests.scala
@@ -1,6 +1,7 @@
 package org.toxicblend.tests
 
 import toxi.geom.Vec3D
+import org.toxicblend.geometry.LineSegment
 import org.toxicblend.geometry.RamerDouglasPeuckerAlgorithm.simplify
 import org.scalatest._
 import matchers.ShouldMatchers._
@@ -9,6 +10,18 @@ import matchers.ShouldMatchers._
  * 
  */
 class SimplifyTests extends FlatSpec with Matchers {
+  
+  "LineSegment-1" should "calculate correct distance" in {    
+    val segment = new LineSegment(new Vec3D(0f,-1f,1f), new Vec3D(-1f,0f,1f))
+    val p = new Vec3D(1f,-1f,1f)
+    segment.distanceSquared(p) should be (1f)
+  }
+  
+  "LineSegment-2" should "calculate correct distance" in {    
+    val segment = new LineSegment(new Vec3D(0f,-1f,1f), new Vec3D(-1f,0f,1f))
+    val p = new Vec3D(-1f,-1f,1f)
+    segment.distanceSquared(p) should be (1f)
+  }
   
   "SimplifyTests-1" should "simplify nothing" in {    
     val segment = Array(new Vec3D(0f,-1f,1f), 
