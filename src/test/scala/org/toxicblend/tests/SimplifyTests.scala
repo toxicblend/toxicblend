fb6fcea83e5823a834dc23514605765321c56c58
EAD Fritz <ead.fritz@gmail.com>
2014-04-06 18:41:37 +0200

    Renamed the LineSegment class

--- src/test/scala/org/toxicblend/tests/SimplifyTests.scala
+++ src/test/scala/org/toxicblend/tests/SimplifyTests.scala
@@ -1,7 +1,7 @@
 package org.toxicblend.tests
 
 import toxi.geom.Vec3D
-import org.toxicblend.geometry.LineSegment
+import org.toxicblend.geometry.QuickLineSegment
 import org.toxicblend.geometry.RamerDouglasPeuckerAlgorithm.simplify
 import org.scalatest._
 import matchers.ShouldMatchers._
@@ -12,23 +12,25 @@ import matchers.ShouldMatchers._
 class SimplifyTests extends FlatSpec with Matchers {
   
   "LineSegment-1" should "calculate correct distance" in {    
-    val segment = new LineSegment(new Vec3D(0f,-1f,1f), new Vec3D(-1f,0f,1f))
-    val p = new Vec3D(1f,-1f,1f)
+    val segment = new QuickLineSegment(new Vec3D(0f,1f,0f), new Vec3D(0f,-1f,0f))
+    val p = new Vec3D(-1f,0f,0f)
     segment.distanceSquared(p) should be (1f)
   }
   
   "LineSegment-2" should "calculate correct distance" in {    
-    val segment = new LineSegment(new Vec3D(0f,-1f,1f), new Vec3D(-1f,0f,1f))
-    val p = new Vec3D(-1f,-1f,1f)
-    segment.distanceSquared(p) should be (1f)
+    val segment = new QuickLineSegment(new Vec3D(0f,0f,0f), new Vec3D(1f,0f,0f))
+    val p = new Vec3D(2f,0f,0f)
+    val d = segment.distanceSquared(p)
+    d should not be (Float.NaN)
+    d should be (1f)
   }
   
   "SimplifyTests-1" should "simplify nothing" in {    
-    val segment = Array(new Vec3D(0f,-1f,1f), 
-                        new Vec3D(-1f,0f,1f),
-                        new Vec3D(0f,1f,1f),
-                        new Vec3D(1f,0f,1f),
-                        new Vec3D(0f,-1f,1f))                                       
+    val segment = Array(new Vec3D(0f,-1f,0f), 
+                        new Vec3D(-1f,0f,0f),
+                        new Vec3D(0f,1f,0f),
+                        new Vec3D(1f,0f,0f),
+                        new Vec3D(0f,-1f,0f))                                       
     val result = simplify(segment, 0.1f)
     result.size should be (segment.size)
     
