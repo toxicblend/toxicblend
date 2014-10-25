f4a2c5676029e2d706ff3639d9501b4678030e77
EAD Fritz <ead.fritz@gmail.com>
2014-10-25 18:38:05 +0200

    now using proper properties

--- src/main/blender_addon/toxicblend_generatemesh.py
+++ src/main/blender_addon/toxicblend_generatemesh.py
@@ -7,32 +7,25 @@ bl_info = {
   "name": "Toxicblend - Generate Mesh",
   'description': 'Toxiclibs.generatemesh: Generates mesh from edge rings',
   'author': 'EAD Fritz',
-  'blender': (2, 71, 0),
+  'blender': (2, 72, 0),
   "category": "Object",
 }
        
 class ToxicBlend_GenerateMesh(bpy.types.Operator):
-  '''Calls toxiclibs.generatemesh '''
+  '''Calls toxicblend.generatemesh '''
   bl_idname = "object.toxicblend_meshgenerator"
   bl_label = "Toxicblend:GenerateMesh"
   bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
-  
+  radiusProperty = bpy.props.FloatProperty(name="Radius [mm]", description="The radius of the curvature (in mm)", default=2.0, min=0.0, max=10000.0)
   useMultiThreadingProperty = bpy.props.EnumProperty(
     description="Each continous ring segment will be processed in a separate thread",
     name="Use mulithreading algorithm",
     items=(("TRUE", "True",""),
            ("FALSE", "False","")),
-           default="FALSE"    
-          )
-  useToOutlineProperty = bpy.props.EnumProperty(
-    description="Execute Polygon2D.toOutline on the result (toxiclibs). Useful when there are self-intersecting loops",
-    name="Use toOutline",
-    items=(("TRUE", "True",""),
-           ("FALSE", "False","")),
-           default="FALSE"    
-          )
-                    
-  radiusProperty = bpy.props.FloatProperty(name="Radius [mm]", description="The radius of the curvature (in mm)", default=2.0, min=0.0, max=10000.0)
+           default="FALSE" )
+           
+  subdivisionProperty = bpy.props.IntProperty(name="subdivisions", description="The number of subdivisions", default=5, min=1, max=100)
+  
 
   @classmethod
   def poll(cls, context):
@@ -44,8 +37,8 @@ class ToxicBlend_GenerateMesh(bpy.types.Operator):
       with toxicblend.ByteCommunicator("localhost", 9999) as bc: 
         unitSystemProperty = context.scene.unit_settings
         activeObject = context.scene.objects.active
-        properties = {'useMultiThreading' : str(self.useMultiThreadingProperty),
-                      'useToOutline'      : str(self.useToOutlineProperty),
+        properties = {'useMultiThreading' : self.useMultiThreadingProperty,
+                      'subdivisions'      : str(self.subdivisionProperty),
                       'radius'            : str(self.radiusProperty),
                       'unitSystem'        : str(unitSystemProperty.system), 
                       'unitScale'         : str(unitSystemProperty.scale_length) }
