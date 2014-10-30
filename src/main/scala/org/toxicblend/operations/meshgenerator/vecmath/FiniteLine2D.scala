7332db9c527dfbb06f231786ac2552602d41b661
EAD Fritz <ead.fritz@gmail.com>
2014-10-30 19:49:10 +0100

    Added intersectCoincidentLine

--- src/main/scala/org/toxicblend/operations/meshgenerator/vecmath/FiniteLine2D.scala
+++ src/main/scala/org/toxicblend/operations/meshgenerator/vecmath/FiniteLine2D.scala
@@ -8,6 +8,40 @@ class FiniteLine2D(val a:Vec2D, val b:Vec2D) {
   
   def getDirection: Vec2D = b.sub(a).normalize
   
+  protected def overlapIntervals(ub1:Double, ub2:Double) = {
+    val l = Math.min(ub1, ub2)
+    val r = Math.max(ub1, ub2)
+    val a = Math.max(0, l)
+    val b = Math.min(1, r)
+    if (a > b) // no intersection
+      IndexedSeq[Double]()
+    else if (a == b)
+      IndexedSeq( a )
+    else // if (a < B)
+      IndexedSeq( a, b)
+  }
+
+  // IMPORTANT: a1 and a2 cannot be the same, e.g. a1--a2 is a true segment, not a point
+  // b1/b2 may be the same (b1--b2 is a point)
+  def intersectCoincidentLine(a1:Vec2D, a2:Vec2D, b1:Vec2D, b2:Vec2D) = {
+
+    assert(a1!=a2)
+    val denomx = a2.x - a1.x
+    val denomy = a2.y - a1.y
+
+    val (ub1, ub2) = if (Math.abs(denomx) > Math.abs(denomy)) {
+      ( (b1.x - a1.x) / denomx, (b2.x - a1.x) / denomx)
+    } else {
+      ( (b1.y - a1.y) / denomy, (b2.y - a1.y) / denomy)
+    }
+
+    overlapIntervals(ub1, ub2).map( f=> {
+      val x = a2.x * f + a1.x * (1d - f)
+      val y = a2.y * f + a1.y * (1d - f)
+      Vec2D(x, y)
+    })
+  }
+   
   def intersectLine(l:FiniteLine2D): Option[Intersection] = {
     val denom = (l.b.y - l.a.y) * (b.x - a.x) - (l.b.x - l.a.x) * (b.y - a.y)
     val na = (l.b.x - l.a.x) * (a.y - l.a.y) - (l.b.y - l.a.y) * (a.x - l.a.x)
@@ -22,8 +56,18 @@ class FiniteLine2D(val a:Vec2D, val b:Vec2D) {
         Option(new SimpleIntersection(intersection))
       } else return None
     } else if (na == 0.0 && nb == 0.0) {
-      println("" + this + " is coincident with " + l )
+      val r = intersectCoincidentLine(a, b, l.a, l.b)
+      if (r.size>0) println("" + this + " is coincident with " + l )
+      else println("" + this + " is coincident with " + l + " but no intersection")
       
+      r.size match {
+        case 0 => None
+        case 1 => Option(new SimpleIntersection(r(0)))
+        case 2 => Option(new CoincidentIntersection(r(0), r(1)))
+        case _ => assert(false, "should never happen"); None
+      }
+      
+      /*
       val aTobSquared = a.distanceToSquared(b)
       val aToLaSquared = a.distanceToSquared(l.a)
       val aToLbSquared = a.distanceToSquared(l.b)
@@ -46,9 +90,10 @@ class FiniteLine2D(val a:Vec2D, val b:Vec2D) {
       if (aToLbSquared == aTobSquared ) return Option(new SimpleIntersection(l.b))
       
       assert(false, "no more options left")
-      None
-    } else return None
+      None*/
+    } else None
   }
+  
   def intersects(that:FiniteLine2D): Boolean = FiniteLine2D.intersects(a, b, that.a, that.b)
   def sqrDistanceToPoint(p:Vec2D) = FiniteLine2D.sqrDistanceToPoint(p,a,b)
   def distanceToPoint(p:Vec2D) = math.sqrt(FiniteLine2D.sqrDistanceToPoint(p,a,b))
