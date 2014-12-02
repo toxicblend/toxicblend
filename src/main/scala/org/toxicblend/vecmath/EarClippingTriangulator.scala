504369654f092e0129a0b46203138e80620429f4
EAD Fritz <ead.fritz@gmail.com>
2014-12-02 14:52:58 +0100

    cleanup

--- src/main/scala/org/toxicblend/vecmath/EarClippingTriangulator.scala
+++ src/main/scala/org/toxicblend/vecmath/EarClippingTriangulator.scala
@@ -4,7 +4,7 @@ import org.toxicblend.util.CyclicDoubleLinkedArray
 import org.toxicblend.util.LinearDoubleLinkedArray
 
 /**
- * Implements ear clipping triangulation
+ * Implements ear clipping triangulation.
  * Class is not thread safe, but as long as each thread has it's own instance it will be safe to use
  */
 class EarClippingTriangulator {
@@ -12,7 +12,6 @@ class EarClippingTriangulator {
   val vertices = new CyclicDoubleLinkedArray
   val earTips = new LinearDoubleLinkedArray
   val reflexVertices = new LinearDoubleLinkedArray
-  //val convexVertices = new LinearDoubleLinkedArray
   val rv = new collection.mutable.ArrayBuffer[Array[Int]]
   
   def isEarTip(i:Int, reflexVerticesHead:Int):Boolean = {
@@ -20,29 +19,24 @@ class EarClippingTriangulator {
     var r = reflexVerticesHead
     val ip = vertices.prev(i)
     val in = vertices.next(i)
+    if (ip == in) return false
     val p0 = input(ip)
     val p1 = input(i)
     val p2 = input(in)
     while (r >= 0) {
       if (r!=ip && r!=i && r!= in && Polygon2D.pointInTriangle(input(r), p0, p1, p2)){
-        //println("p=" + input(r) + " is inside " + p0 + "->" + p1 + "->" + p2 )
         return false
-      } //else println("p=" + input(r) + " is NOT inside " + p0 + "->" + p1 + "->" + p2 )
+      } 
       r = reflexVertices.next(r)
     }
     true
   }
   
-  
   def testAngleOfPoint(p:Int) {
     if (Vec2D.ccw(input(vertices.prev(p)), input(p), input(vertices.next(p))) < 0) {
-      //println("" + p + " is convex")
       reflexVertices.safeDrop(p)
-      //convexVertices.safeAdd(p)
     } else {
-      //println("" + p + " is reflex")
       reflexVertices.safeAdd(p)
-      //convexVertices.safeDrop(p)
     }
   }
   
@@ -61,15 +55,11 @@ class EarClippingTriangulator {
       val vp = vertices.prev(i)
       val vn = vertices.next(i)
       if (vp == vn) {
-        //val v = vertices.toIndexedSeq
-        //if (v.size > 0) println("early return: " + v.mkString(","))
         return true
       } else {
         rv.append(Array(vp, i, vn))
-        //println("added triangle: " + rv.last.mkString(","))
         vertices.drop(i)
-        //reflexVertices.safeDrop(i)
-        //convexVertices.safeDrop(i)
+        reflexVertices.safeDrop(i)
         //println("removed " + i + " from further processing")
         
         // a convex vertex can never become reflex, so there is no need to test those
@@ -81,7 +71,10 @@ class EarClippingTriangulator {
         i = earTips.getOne
       }
     }
-    !vertices.isEmpty
+    {
+      var i = vertices.getOne
+      !(vertices.isEmpty || (vertices.next(vertices.next(i)) == i)  )
+    }
   }
   
   def triangulatePolygon(input:IndexedSeq[Vec2D]):IndexedSeq[Array[Int]] = {
@@ -98,17 +91,10 @@ class EarClippingTriangulator {
     vertices.setup(size)
     earTips.setup(size)
     reflexVertices.setup(size, empty=true)
-    //convexVertices.setup(size, empty=true)
-    
-    //println("reflexVertices =" + reflexVertices.indices.map(i=>"p="+i.prev +",n="+i.next).mkString(","))
-    //println("convexVertices =" + convexVertices.indices.map(i=>"p="+i.prev +",n="+i.next).mkString(","))
-    
+  
     for(i<-0 until size) 
       testAngleOfPoint(i)
-      
-    //println("reflexVertices =" + reflexVertices.indices.map(i=>"p="+i.prev +",n="+i.next).mkString(","))
-    //println("convexVertices =" + convexVertices.indices.map(i=>"p="+i.prev +",n="+i.next).mkString(","))
-    
+        
     var i = earTips.head
     val rhead = reflexVertices.head 
     while (i >= 0) {
@@ -122,7 +108,6 @@ class EarClippingTriangulator {
       println("vertices       =" + vertices.toIndexedSeq.mkString(","))
       println("earTips        =" + earTips.toIndexedSeq.mkString(","))
       println("reflexVertices =" + reflexVertices.toIndexedSeq.mkString(","))
-      //println("convexVertices =" + convexVertices.toIndexedSeq.mkString(","))
       println
     }
         
