df79aa100250310539d41b5c79b7beaba9854d51
EAD Fritz <ead.fritz@gmail.com>
2014-12-02 14:54:17 +0100

    Added SutherlandHodgemanRectangularClipper

--- src/main/scala/org/toxicblend/vecmath/SutherlandHodgemanClipper.scala
+++ src/main/scala/org/toxicblend/vecmath/SutherlandHodgemanClipper.scala
@@ -5,12 +5,13 @@ import SutherlandHodgemanClipper.intersection
 
 class SutherlandHodgemanClipper {
  
-  def isInside(p:Vec2D, a:Vec2D, b:Vec2D) = (a.x-p.x)*(b.y-p.y)>(a.y-p.y)*(b.x-p.x)
-  
   def clipPolygon(input:IndexedSeq[Vec2D], a:Vec2D, b:Vec2D, ε:Double):IndexedSeq[Vec2D] = {
     
+    @inline def isInside(p:Vec2D) = (a.x-p.x)*(b.y-p.y)>(a.y-p.y)*(b.x-p.x)
+    
     @inline def conditionalAppend(buffer:ArrayBuffer[Vec2D], v:Vec2D) = {
-      if (buffer.size==0 || !buffer.last.=~=(v,ε) ) buffer.append(v)
+      if (buffer.size==0 || !buffer.last.=~=(v,ε) ) 
+        buffer.append(v)
       buffer
     } 
     
@@ -19,7 +20,7 @@ class SutherlandHodgemanClipper {
     val rv = (0 until input.size).foldLeft(new ArrayBuffer[Vec2D]) ((x,i) => {
       val current = input(i)
       val previous = input(previousI)//input((i+input.size-1)%input.size)
-      (isInside(current, a, b),isInside(previous, a, b)) match {
+      (isInside(current),isInside(previous)) match {
         case (true,true) => conditionalAppend(x,current)
         case (true,false) => conditionalAppend(conditionalAppend(x,intersection(a,b,current,previous)),current)
         case (false,true) => conditionalAppend(x,intersection(a,b,current,previous))
@@ -42,6 +43,43 @@ class SutherlandHodgemanClipper {
   }
 }
 
+class SutherlandHodgemanRectangularClipper {
+   
+  def clipPolygon(input:IndexedSeq[Vec2D], a:Vec2D, b:Vec2D, aabb:AABB2D, ε:Double):IndexedSeq[Vec2D] = {
+    
+    @inline def isInside(p:Vec2D) = aabb.containsPoint(p, ε)
+    
+    @inline def conditionalAppend(buffer:ArrayBuffer[Vec2D], v:Vec2D) = {
+      if (buffer.size==0 || !buffer.last.=~=(v,ε) ) 
+        buffer.append(v)
+      buffer
+    } 
+    
+    var previousI = input.size-1
+    //println("Clipping : " + a + " -> " + b )
+    val rv = (0 until input.size).foldLeft(new ArrayBuffer[Vec2D]) ((x,i) => {
+      val current = input(i)
+      val previous = input(previousI)//input((i+input.size-1)%input.size)
+      (isInside(current),isInside(previous)) match {
+        case (true,true) => conditionalAppend(x,current)
+        case (true,false) => conditionalAppend(conditionalAppend(x,intersection(a,b,current,previous)),current)
+        case (false,true) => conditionalAppend(x,intersection(a,b,current,previous))
+        case _ => 
+      }
+      previousI = i
+      x
+    })
+    if ( rv.size >1 && rv.last.=~=(rv.head,ε)) rv.remove(rv.size-1)
+    rv
+  }
+    
+  def clipPolygon(input:IndexedSeq[Vec2D], aabb:AABB2D, ε:Double):IndexedSeq[Vec2D] = {
+    val clipEdges = aabb.toIndexedSeq(true)
+    val clipSize = 4
+    (0 until clipSize).foldLeft(input)((p,i) => clipPolygon(p, clipEdges((i+clipSize-1) % clipSize), clipEdges(i), aabb, ε))
+  }
+}
+
 object SutherlandHodgemanClipper {
   
   def intersection(a:Vec2D, b:Vec2D, p:Vec2D, q:Vec2D) = {
@@ -56,7 +94,10 @@ object SutherlandHodgemanClipper {
   }
      
   lazy val singleton = new SutherlandHodgemanClipper
+  lazy val singletonRectangular = new SutherlandHodgemanRectangularClipper
   def clip(subject:Polygon2D, clipPolygon:Polygon2D, enforceDirection:Option[Boolean]=None, ε:Double=Polygon2D.ε)=Polygon2D(singleton.clipPolygon(subject.toIndexedSeq, clipPolygon.toIndexedSeq,ε), enforceDirection, ε)
+  def clipRect(subject:Polygon2D, aabb:AABB2D, enforceDirection:Option[Boolean]=None, ε:Double=Polygon2D.ε)=Polygon2D(singletonRectangular.clipPolygon(subject.toIndexedSeq, aabb,ε), enforceDirection, ε)
+
   def clip(subject:IndexedSeq[Vec2D], clipEdges:IndexedSeq[Vec2D], ε:Double)=singleton.clipPolygon(subject, clipEdges, ε)
   def clip(subject:IndexedSeq[Vec2D], clipEdge:FiniteLine2D, ε:Double)=singleton.clipPolygon(subject, clipEdge, ε)
   def clip(subject:IndexedSeq[Vec2D], clipV1:Vec2D, clipV2:Vec2D, ε:Double)=singleton.clipPolygon(subject, clipV1, clipV2, ε)
