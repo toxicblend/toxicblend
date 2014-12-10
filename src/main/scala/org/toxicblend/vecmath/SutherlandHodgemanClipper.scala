50fc9f18a42fd3f21226c228e174c27777ee4594
ead <ead.fritz@gmail.com>
2014-12-10 21:44:50 +0100

    cleanup

--- src/main/scala/org/toxicblend/vecmath/SutherlandHodgemanClipper.scala
+++ src/main/scala/org/toxicblend/vecmath/SutherlandHodgemanClipper.scala
@@ -1,7 +1,7 @@
 package org.toxicblend.vecmath
 
 import scala.collection.mutable.ArrayBuffer
-import SutherlandHodgemanClipper.intersection
+import SutherlandHodgemanClipper.{intersection,conditionalAppend}
 
 class SutherlandHodgemanClipper {
  
@@ -9,21 +9,14 @@ class SutherlandHodgemanClipper {
     
     @inline def isInside(p:Vec2D) = (a.x-p.x)*(b.y-p.y)>(a.y-p.y)*(b.x-p.x)
     
-    @inline def conditionalAppend(buffer:ArrayBuffer[Vec2D], v:Vec2D) = {
-      if (buffer.size==0 || !buffer.last.=~=(v,ε) ) 
-        buffer.append(v)
-      buffer
-    } 
-    
     var previousI = input.size-1
-    //println("Clipping : " + a + " -> " + b )
     val rv = (0 until input.size).foldLeft(new ArrayBuffer[Vec2D]) ((x,i) => {
       val current = input(i)
-      val previous = input(previousI)//input((i+input.size-1)%input.size)
+      val previous = input(previousI)
       (isInside(current),isInside(previous)) match {
-        case (true,true) => conditionalAppend(x,current)
-        case (true,false) => conditionalAppend(conditionalAppend(x,intersection(a,b,current,previous)),current)
-        case (false,true) => conditionalAppend(x,intersection(a,b,current,previous))
+        case (true,true)  => conditionalAppend(x,current,ε)
+        case (true,false) => conditionalAppend(conditionalAppend(x,intersection(a,b,current,previous),ε),current,ε)
+        case (false,true) => conditionalAppend(x,intersection(a,b,current,previous),ε)
         case _ => 
       }
       previousI = i
@@ -49,21 +42,14 @@ class SutherlandHodgemanRectangularClipper {
     
     @inline def isInside(p:Vec2D) = aabb.containsPoint(p, ε)
     
-    @inline def conditionalAppend(buffer:ArrayBuffer[Vec2D], v:Vec2D) = {
-      if (buffer.size==0 || !buffer.last.=~=(v,ε) ) 
-        buffer.append(v)
-      buffer
-    } 
-    
     var previousI = input.size-1
-    //println("Clipping : " + a + " -> " + b )
     val rv = (0 until input.size).foldLeft(new ArrayBuffer[Vec2D]) ((x,i) => {
       val current = input(i)
-      val previous = input(previousI)//input((i+input.size-1)%input.size)
+      val previous = input(previousI)
       (isInside(current),isInside(previous)) match {
-        case (true,true) => conditionalAppend(x,current)
-        case (true,false) => conditionalAppend(conditionalAppend(x,intersection(a,b,current,previous)),current)
-        case (false,true) => conditionalAppend(x,intersection(a,b,current,previous))
+        case (true,true)  => conditionalAppend(x,current,ε)
+        case (true,false) => conditionalAppend(conditionalAppend(x,intersection(a,b,current,previous),ε),current,ε)
+        case (false,true) => conditionalAppend(x,intersection(a,b,current,previous),ε)
         case _ => 
       }
       previousI = i
@@ -92,12 +78,18 @@ object SutherlandHodgemanClipper {
     val det = a1 * b2 - a2 * b1
     Vec2D((b2 * c1 - b1 * c2) / det, (a1 * c2 - a2 * c1) / det)
   }
-     
+  
+  @inline def conditionalAppend(buffer:ArrayBuffer[Vec2D], v:Vec2D, ε:Double) = {
+    if (buffer.size==0 || !buffer.last.=~=(v,ε) ) 
+      buffer.append(v)
+    buffer
+  }
+  
   lazy val singleton = new SutherlandHodgemanClipper
   lazy val singletonRectangular = new SutherlandHodgemanRectangularClipper
   def clip(subject:Polygon2D, clipPolygon:Polygon2D, enforceDirection:Option[Boolean]=None, ε:Double=Polygon2D.ε)=Polygon2D(singleton.clipPolygon(subject.toIndexedSeq, clipPolygon.toIndexedSeq,ε), enforceDirection, ε)
   def clipRect(subject:Polygon2D, aabb:AABB2D, enforceDirection:Option[Boolean]=None, ε:Double=Polygon2D.ε)=Polygon2D(singletonRectangular.clipPolygon(subject.toIndexedSeq, aabb,ε), enforceDirection, ε)
-
+ 
   def clip(subject:IndexedSeq[Vec2D], clipEdges:IndexedSeq[Vec2D], ε:Double)=singleton.clipPolygon(subject, clipEdges, ε)
   def clip(subject:IndexedSeq[Vec2D], clipEdge:FiniteLine2D, ε:Double)=singleton.clipPolygon(subject, clipEdge, ε)
   def clip(subject:IndexedSeq[Vec2D], clipV1:Vec2D, clipV2:Vec2D, ε:Double)=singleton.clipPolygon(subject, clipV1, clipV2, ε)
