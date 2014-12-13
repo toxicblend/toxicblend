a9ea4ab36431a6d5f86ec4a54b545acc62d178a7
ead <ead.fritz@gmail.com>
2014-12-13 20:15:03 +0100

    Added a multithreaded constructor of little use

--- src/main/scala/org/toxicblend/vecmath/AABB2D.scala
+++ src/main/scala/org/toxicblend/vecmath/AABB2D.scala
@@ -1,6 +1,7 @@
 package org.toxicblend.vecmath
 
 import scala.IndexedSeq
+import org.toxicblend.util.IntNumberUtils
 
 class AABB2D protected (val min:Vec2D, val max:Vec2D) extends ImmutableVec2D((max.x-min.x)/2d+min.x, (max.y-min.y)/2d+min.y ) {
   //if(max.x!= )assert(max.x >= min.x)
@@ -52,28 +53,63 @@ class AABB2D protected (val min:Vec2D, val max:Vec2D) extends ImmutableVec2D((ma
   def toPolygon2D(clockwise:Boolean=false)=Polygon2D(toIndexedSeq(clockwise))
 }
 
+
+
 object AABB2D {
   
-  @inline protected def growToContainPoint(point:Vec2D, min:MutableVec2D, max:MutableVec2D) = {
-    if (point.x > max.x) max.x = point.x
-    if (point.x < min.x) min.x = point.x
-    if (point.y > max.y) max.y = point.y
-    if (point.y < min.y) min.y = point.y
+  private class MutableAABB2DContainer(val min:MutableVec2D, val max:MutableVec2D) {
+    def this() = this(MutableVec2D(Double.PositiveInfinity, Double.PositiveInfinity), MutableVec2D(Double.NegativeInfinity, Double.NegativeInfinity ) )
+    
+    def growToContainAABB(other:MutableAABB2DContainer) = {
+      growToContainPoint(other.min)
+      growToContainPoint(other.max)
+      this
+    }
+    
+    def growToContainPoint(point:Vec2D) = {
+      if (point.x > max.x) max.x = point.x
+      if (point.x < min.x) min.x = point.x
+      if (point.y > max.y) max.y = point.y
+      if (point.y < min.y) min.y = point.y
+    }
+  }  
+  
+  /**
+   * a pointless parallel constructor. The non-parallel version is still faster when n=41K vertices
+   */
+  def parallelConstructor(vertices:IndexedSeq[Vec2D]):AABB2D = {
+    
+    val size = vertices.size
+    val perThread = (0.5 + size.toDouble / Runtime.getRuntime().availableProcessors().toDouble).toInt
+
+    val jobs = for (i <- 0 until size by perThread ) yield i
+    
+    val seqOp=(aabb:MutableAABB2DContainer, b:Int) => {
+      val endElement = IntNumberUtils.min(b + perThread, size)
+      for (i <- b until endElement ) 
+        aabb.growToContainPoint(vertices(i))
+      aabb
+    }
+    
+    val combOp=(a:MutableAABB2DContainer,b:MutableAABB2DContainer) => a.growToContainAABB(b)
+    
+    val aabb = jobs.par.aggregate(new MutableAABB2DContainer) (seqOp,combOp)
+    apply(aabb.min, aabb.max)
   }
   
-  def apply(vertices:Seq[Vec2D]) = {
-    val min = new MutableVec2D(Double.PositiveInfinity, Double.PositiveInfinity)
-    val max = new MutableVec2D(Double.NegativeInfinity, Double.NegativeInfinity)
-    vertices.foreach(v => growToContainPoint(v, min, max))
-    new AABB2D(min,max)
+  def apply(aabb:MutableAABB2DContainer):AABB2D = new AABB2D(Vec2D(aabb.min),Vec2D(aabb.max))
+  
+  def apply(vertices:Seq[Vec2D]):AABB2D = {
+    val aabb = new MutableAABB2DContainer
+    vertices.foreach(v => aabb.growToContainPoint(v))
+    apply(aabb)
   }
   
-  def apply(v1:Vec2D, v2:Vec2D) = {
-    val min = new MutableVec2D(Double.PositiveInfinity, Double.PositiveInfinity)
-    val max = new MutableVec2D(Double.NegativeInfinity, Double.NegativeInfinity)
-    growToContainPoint(v1, min, max)
-    growToContainPoint(v2, min, max)
-    new AABB2D(min,max)
+  def apply(v1:Vec2D, v2:Vec2D):AABB2D = {
+    val aabb = new MutableAABB2DContainer
+    aabb.growToContainPoint(v1)
+    aabb.growToContainPoint(v2)
+    apply(aabb)
   }
   
   def apply(x1:Double, y1:Double, x2:Double, y2:Double) = {
