23d58c9143e270042981a368ebe69d6b426d3617
ead <ead.fritz@gmail.com>
2014-12-28 23:39:51 +0100

    Now logging the entire client socketAdress. Auto-reindent by eclipse.

--- src/main/scala/org/toxicblend/ToxicblendServer.scala
+++ src/main/scala/org/toxicblend/ToxicblendServer.scala
@@ -4,29 +4,29 @@ import java.io.IOException
 import java.net.ServerSocket
 import java.net.Socket
 import java.io.DataInputStream
-import java.net.SocketTimeoutException 
+import java.net.SocketTimeoutException
 import akka.actor.ActorSystem
 
 /**
  * The main class for the toxic blend server
  */
 object ToxicblendServer {
-  
-  class SocketListener(val actorSystem:ActorSystem, val port:Int) extends Thread("SocketListener") {
+
+  class SocketListener(val actorSystem: ActorSystem, val port: Int) extends Thread("SocketListener") {
     var quit = false
     var isRunning = true
-   
+
     override def run(): Unit = {
       try {
-        val socket:ServerSocket = new ServerSocket(port)
+        val socket: ServerSocket = new ServerSocket(port)
         socket.setSoTimeout(1000)
         println("Started service, listening to port " + port)
         while (!quit) {
           try {
-            val aSocket:Socket = socket.accept
+            val aSocket: Socket = socket.accept
             aSocket.setSoTimeout(0)
-            println("Opening connection to " + aSocket.getPort() + "->" + aSocket.getLocalPort)
-            new ServerThread(actorSystem, aSocket).start 
+            println("Opening connection to " + aSocket.getRemoteSocketAddress + "->" + aSocket.getLocalPort)
+            new ServerThread(actorSystem, aSocket).start
           } catch {
             case e: SocketTimeoutException => // println("ignoring " + e)
           }
@@ -42,20 +42,20 @@ object ToxicblendServer {
       isRunning = false
     }
   }
-  
+
   def main(args: Array[String]): Unit = {
     val port = 9999
     //val keyboard = new Scanner(System.in)
-    val keyboard=new DataInputStream(System.in);
+    val keyboard = new DataInputStream(System.in);
     val system = ActorSystem("ToxicBlend")
-    
+
     val socketListener = new SocketListener(system, port)
     socketListener.start
-    var inputChar= ' '
-    while (socketListener.isRunning && inputChar!='q' && inputChar!='Q' ) {      
+    var inputChar = ' '
+    while (socketListener.isRunning && inputChar != 'q' && inputChar != 'Q') {
       inputChar = keyboard.readByte.toChar
     }
-    if (socketListener.isRunning ) {
+    if (socketListener.isRunning) {
       println("Existing, waiting for socket threads to terminate")
       socketListener.quit = true
       socketListener.join
